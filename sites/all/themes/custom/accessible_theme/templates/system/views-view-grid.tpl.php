6be2ee458683d777154b36f6b7f815ea492fba67
kevintnguyens <kevitn11@uci.edu>
2015-12-16 11:00:56 -0800

    did it

--- sites/all/themes/custom/accessible_theme/templates/system/views-view-grid.tpl.php
+++ sites/all/themes/custom/accessible_theme/templates/system/views-view-grid.tpl.php
@@ -10,15 +10,12 @@
  * @ingroup views_templates
  */
 ?>
-<?php
-  echo ("ok");
 
-?>
 
 <?php if (!empty($title)) : ?>
   <h3><?php print $title; ?></h3>
 <?php endif; ?>
-<table class="<?php print "Grid"; ?>"<?php print $attributes; ?>>
+<table class="<?php print "Grid ".$class; ?>"<?php print $attributes; ?>>
   <?php if (!empty($caption)) : ?>
     <caption><?php print $caption; ?></caption>
   <?php endif; ?>
@@ -27,7 +24,7 @@
     <?php foreach ($rows as $row_number => $columns): ?>
       <tr <?php if ($row_classes[$row_number]) { print 'class="' . $row_classes[$row_number] .'"';  } ?>>
         <?php foreach ($columns as $column_number => $item): ?>
-          <td <?php if ($column_classes[$row_number][$column_number]) { print 'class="' . $column_classes[$row_number][$column_number] .'"';  } ?>>
+          <td <?php if ($column_classes[$row_number][$column_number]) { print 'class="' . $column_classes[$row_number][$column_number] .' Grid-cell"';  } ?>>
             <?php print $item; ?>
           </td>
         <?php endforeach; ?>
